b7b86926c7ee2a42f01dacc130c955546c29dc4a
CDPC <chanchrisdominic@gmail.com>
2021-10-03 17:10:51 +0800

    added functionality approxEQ & randomBools

--- src/util/Color.ts
+++ src/util/Color.ts
@@ -1,3 +1,5 @@
+import { approxEQ, clamp, getRandBool } from "./clamp"
+
 export class Color {
 
     r: number
@@ -5,16 +7,22 @@ export class Color {
     b: number
     a: number
 
-    constructor(red: number, green: number, blue: number, alpha: number) {
-        this.r = red
-        this.g = green
-        this.b = blue
-        this.a = alpha
+    private static calculateHue(r: number, g: number, b: number, max: number, delta: number) {
+        let hue: number 
+
+        if(approxEQ(r, max)) {
+            hue = 60 * (((g - b)/delta)%6)
+        } else if(approxEQ(g, max)) {
+            hue = 60 * (((b - r)/delta) + 2)
+        } else if(approxEQ(b, max)) {
+            hue = 60 * (((r - g)/delta) + 4)
+        } else {
+            hue = 0
+        }
+
+        return hue
     }
-
-    static WHITE = new Color(255, 255, 255, 1)
-    static BLACK = new Color(0, 0, 0, 1)
-
+    
     public static genRandColor(mix?: Color) {
         let red = Math.random() * 255
         let green = Math.random() * 255
@@ -27,6 +35,16 @@ export class Color {
         }
 
         return new Color(red, green, blue, 1)
+    }
+
+    static WHITE = new Color(255, 255, 255, 1)
+    static BLACK = new Color(0, 0, 0, 1)
+
+    constructor(red: number, green: number, blue: number, alpha: number) {
+        this.r = red
+        this.g = green
+        this.b = blue
+        this.a = alpha
     }
 
     mix(mix: Color) {
@@ -45,6 +63,29 @@ export class Color {
         return new Color(r, g, b, 1)
     }
 
+    setAlpha(alpha: number) {
+        this.a = alpha
+
+        return this
+    }
+
+    toHSL() {
+        let normalizedR = this.r/255
+        let normalizedG = this.g/255
+        let normalizedB = this.b/255
+
+        let cMax = Math.max(normalizedR, normalizedG, normalizedB)
+        let cMin = Math.min(normalizedR, normalizedG, normalizedB)
+
+        let delta = cMax - cMin
+
+        let lightness = (cMax + cMin)/2
+        let saturation = approxEQ(delta, 0) ? 0 : delta/(1 - Math.abs((2 * lightness) - 1))
+        let hue = Color.calculateHue(normalizedR, normalizedG, normalizedB, cMax, delta)
+
+        return new ColorHSL(hue, saturation * 100, lightness * 100)
+    }
+
     brighter() {
         return this.mix(Color.WHITE)
     }
@@ -55,5 +96,54 @@ export class Color {
 
     toString() {
         return `rgba(${this.r}, ${this.g}, ${this.b}, ${this.a})`
+    }
+}
+
+export class ColorHSL {
+    private h
+    private s
+    private l
+
+    constructor(hue: number, saturation: number, lightness: number) {
+        this.h = hue
+        this.s = saturation
+        this.l = lightness
+    }
+
+    toString() {
+        return `hsl(${this.h}, ${this.s}%, ${this.l}%)`
+    }
+
+    saturate() {
+        this.s = (this.s + 100)/2
+
+        return this
+    }
+
+    deSaturate() {
+       this.s /= 2 
+
+       return this
+    }
+
+    randSat() {
+        this.s = this.s + (getRandBool() ? (-10 * Math.random()) - 5 : (10 * Math.random()) + 5)
+        this.s = clamp(this.s, 0, 100)
+
+        return this
+    }
+
+    randHue() {
+        this.h = this.h + (getRandBool() ? (-5 * Math.random()) : (5 * Math.random()))
+        this.h = clamp(this.h, 0, 360)
+
+        return this
+    }
+
+    randLightness() {
+        this.l = this.l + (getRandBool() ? (-10 * Math.random()) : (10 * Math.random()))
+        this.l = clamp(this.l, 0, 100)
+
+        return this
     }
 }
--- src/util/clamp.ts
+++ src/util/clamp.ts
@@ -1,3 +1,11 @@
 export function clamp(num: number, min: number, max: number = Infinity) {
     return Math.max(Math.min(num, max), min)
 }
+
+export function approxEQ(num1: number, num2: number, epsilon = 0.001) {
+    return Math.abs(num1 - num2) < epsilon
+}
+
+export function getRandBool() {
+    return Math.random() - 0.5 > 0
+}
